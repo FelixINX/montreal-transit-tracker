4229d6ca9080e5c7fa01cefb8f69bf6a481cd32a
Félix Desjardins <felixinx@gmail.com>
2021-04-04 19:38:40 -0400

    💄 Update frontend to Vite + TailwindCss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.default')
 
-        <title>Transit Tracker for Developpers</title>
-    </head>
-    <body>
-        <a href="https://transittracker.ca">Return to Transit Tracker</a>
-    </body>
-</html>
+@section('body')
+<div class="bg-primary px-6 py-3 text-white text-xl"><b>Transit Tracker</b> for Developers</div>
+<div class="container mx-auto mt-6">
+    <div class="rounded-lg bg-white p-8 text-lg">
+        <p class="mb-2 font-medium">Looking for your regular agencies?</p>
+        <a class="bg-secondary hover:bg-secondary-dark hover:text-white transition-colors px-3 py-1 rounded font-medium" href="https://transittracker.ca">Go to transittracker.ca</a>
+    </div>
+</div>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: {
+      primary: {
+          50: '#eef6fc',
+          DEFAULT: '#2374ab',
+          dark: '#00497b'
+      },
+      secondary: {
+          DEFAULT: '#4dccbd',
+          dark: '#009a8d'  
+      },
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+    },
+  },
+  variants: {},
+  plugins: [],
+}
